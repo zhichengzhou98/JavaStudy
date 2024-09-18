d53ff3866ac06c52336167fb9f45b24e06b18ad3
zc.zhou <zzc@renren.com>
2024-09-18 11:04:18 +0800

    09.18 EnumMap

---
+++ src/main/java/effectiveJava/chapter34/MyEnumMap.java
@@ -0,0 +1,42 @@
+package effectiveJava.chapter34;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.print.attribute.SetOfIntegerSyntax;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zc.zhou
+ * @Description
+ * @create 2024-09-18 10:39
+ */
+@Slf4j
+public class MyEnumMap {
+  enum Season {
+    SPRING,
+    SUMMER,
+    AUTUMN,
+    WINTER;
+  }
+
+  @Test
+  void testFun() {
+    //Map<Season, Set<Integer>> seasonSetMap = new HashMap<>();
+    EnumMap<Season, Set<Integer>> seasonSetMap = new EnumMap<>(Season.class);
+    Set<Integer> setSpring = Set.of(1, 2, 3);
+    Set<Integer> setSummer = Set.of(4, 5, 6);
+    seasonSetMap.put(Season.SPRING, setSpring);
+    seasonSetMap.put(Season.SUMMER, setSummer);
+    log.info("map:{}", seasonSetMap);
+
+    Season[] enumConstants = Season.class.getEnumConstants();
+    log.info("string[]:{}", Arrays.toString(enumConstants));
+  }
+}
